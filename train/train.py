a272d09bbe72122249334f454173c4f57ff7a7f3
csteinmetz1 <csteinmetz1@gmail.com>
2019-06-19 21:37:30 -0400

    some work on tensorboard plotting of images

--- train/train.py
+++ train/train.py
@@ -24,13 +24,13 @@ assert version.parse(tf.__version__).release[0] >= 2, \
 eq_params = pd.read_csv("../data/safe/normalized_eq_params.csv", sep=",", index_col=0)
 
 # only use data points with bright or warm descriptors
-eq_df = eq_params.reset_index()#[eq_params['descriptor'].isin(['bright', 'warm'])].reset_index()
+eq_df = eq_params[eq_params['descriptor'].isin(['bright', 'warm'])].reset_index()
 
 # make train / test split
-x_train = eq_df.values[:1600,2:]
-y_train = eq_df.values[:1600,1]
-x_test  = eq_df.values[1600:,2:]
-y_test  = eq_df.values[1600:,1]
+x_train = eq_df.values[:800,2:]
+y_train = eq_df.values[:800,1]
+x_test  = eq_df.values[800:,2:]
+y_test  = eq_df.values[800:,1]
 
 # inspect training and testing data
 print("Training set   : ", x_train.shape)
@@ -38,36 +38,32 @@ print("Traing labels  : ", y_train.shape)
 print("Testing set    : ", x_test.shape)
 print("Testing labels : ", y_test.shape)
 
-autoencoder, encoder, decoder = build_multiple_layer_variational_autoencoder(2, x_train.shape[1])
+autoencoder, encoder, decoder = build_single_layer_variational_autoencoder(2, x_train.shape[1])
 
 def make_plots(epoch, logs):
-    if epoch % 100 == 0:
-
-        # single example for reconstruction testing
-        x = np.array([6.09, 114.77, 3.65, 192.036, 0.23, -12, 915.82, 1.32, -2.13, 444.72, 0.71, -12, 2857.14])
-        x = normalize_params(x).reshape(1,13)
+    if (epoch+1) % 100 == 0:
+        
+        x = np.array([x_test[2,:]])
         z = encoder.predict(x)
-        y = decoder.predict(z[2])
+        y = decoder.predict(z)
         x_hat = denormalize_params(y[0])
-
       
-        compare = compare_tf(x[0], y[0], to_file=os.path.join(logdir,f"reconstruction_{epoch}")) 
+        compare = compare_tf(x[0], y[0], to_file=os.path.join(logdir,f"reconstruction_{epoch+1}")) 
         buf = io.BytesIO()
         compare.savefig(buf, format='png')
-        buf.seek(0)
 
-        #d = {'warm': 0, 'bright': 1}
-        #labels = eq_df['descriptor'][800:].map(d, na_action='ignore').values
-        #models = (encoder, decoder)
-        #data = (x_test, labels)
-        #plot_2d_manifold(models, dim=15, data=data, to_file=os.path.join(plotdir,'2d_manifold'))
+        d = {'warm': 0, 'bright': 1}
+        labels = eq_df['descriptor'][800:].map(d, na_action='ignore').values
+        models = (encoder, decoder)
+        data = (x_test, labels)
+        plot_2d_manifold(models, dim=15, data=data, to_file=os.path.join(logdir,f"2d_manifold_{epoch+1}_"))
 
         file_writer = tf.summary.create_file_writer(logdir) 
         with file_writer.as_default():
-            image = tf.image.decode_png(buf.getvalue(), channels=0)
-            # Add the batch dimension
-            image = tf.expand_dims(image, 0)
-            tf.summary.image("Training data", image, step=epoch)
+            compare_plot = tf.image.decode_png(buf.getvalue(), channels=0)
+            compare_plot = tf.expand_dims(compare_plot, 0)
+            tf.summary.image("Reconstruction", compare_plot, step=epoch+1)
+
 
 # tensorboard logging setup
 logdir="logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -76,10 +72,11 @@ plotting_callback = tf.keras.callbacks.LambdaCallback(on_epoch_end=make_plots)
 
 # train the model
 autoencoder.fit(x_train, x_train, 
-                  shuffle=True,
-                  validation_data=(x_test,x_test),
-                  batch_size=8, 
-                  epochs=1500,
-                callbacks=[tensorboard_callback, plotting_callback])
+                shuffle=True,
+                validation_data=(x_test,x_test),
+                batch_size=8, 
+                epochs=1500,
+                callbacks=[tensorboard_callback, plotting_callback],
+                verbose=True)
 
 autoencoder.save_weights('../models/vae.h5', save_format='h5')
