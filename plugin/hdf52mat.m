b2abd8f9c0c03a4874d9b5de80b891ce1ccfb394
csteinmetz1 <csteinmetz1@gmail.com>
2019-06-22 21:20:35 -0400

    chaning to function format

--- plugin/hdf52mat.m
+++ plugin/hdf52mat.m
@@ -1,3 +1,4 @@
+function hdf52mat
 filename1d = '../models/vae1d.h5';
 filename2d = '../models/vae2d.h5';
 filename3d = '../models/vae3d.h5';
@@ -22,3 +23,4 @@ W2 = double(h5read(filename3d,'/decoder/dense_4/kernel:0').');
 b2 = double(h5read(filename3d,'/decoder/dense_4/bias:0').');
 
 save 'decoder3d.mat' W1 b1 W2 b2
+end
