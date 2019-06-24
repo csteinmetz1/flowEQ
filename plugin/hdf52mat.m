a974f0fa97ff9a2600abbdb72b01fc840b086dc0
csteinmetz1 <csteinmetz1@gmail.com>
2019-06-23 22:29:24 -0400

    updating syntax

--- plugin/hdf52mat.m
+++ plugin/hdf52mat.m
@@ -1,4 +1,5 @@
 function hdf52mat
+
 filename1d = '../models/vae1d.h5';
 filename2d = '../models/vae2d.h5';
 filename3d = '../models/vae3d.h5';
@@ -8,18 +9,18 @@ b1 = double(h5read(filename1d,'/decoder/dense_3/bias:0').');
 W2 = double(h5read(filename1d,'/decoder/dense_4/kernel:0').');
 b2 = double(h5read(filename1d,'/decoder/dense_4/bias:0').');
 
-save 'decoder1d.mat' W1 b1 W2 b2 
+save('decoder1d.mat', 'W1', 'b1', 'W2', 'b2')
 
 W1 = double(h5read(filename2d,'/decoder/dense_3/kernel:0').');
 b1 = double(h5read(filename2d,'/decoder/dense_3/bias:0').');
 W2 = double(h5read(filename2d,'/decoder/dense_4/kernel:0').');
 b2 = double(h5read(filename2d,'/decoder/dense_4/bias:0').');
 
-save 'decoder2d.mat' W1 b1 W2 b2 
+save('decoder2d.mat', 'W1', 'b1', 'W2', 'b2')
 
 W1 = double(h5read(filename3d,'/decoder/dense_3/kernel:0').');
 b1 = double(h5read(filename3d,'/decoder/dense_3/bias:0').');
 W2 = double(h5read(filename3d,'/decoder/dense_4/kernel:0').');
 b2 = double(h5read(filename3d,'/decoder/dense_4/bias:0').');
 
-save 'decoder3d.mat' W1 b1 W2 b2
+save('decoder3d.mat', 'W1', 'b1', 'W2', 'b2')
