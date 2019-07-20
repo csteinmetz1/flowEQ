b8f647c4525710ff7a209a7e9f6df98e42714312
csteinmetz1 <csteinmetz1@gmail.com>
2019-07-20 18:03:49 -0400

    adjustments to the visualizer to make things a little nicer

--- plugin/UDPVisualizer.m
+++ plugin/UDPVisualizer.m
@@ -20,12 +20,12 @@ UDPReceive = dsp.UDPReceiver('LocalIPPort', 20000, ...
                              'ReceiveBufferSize', 49*80);
 c = onCleanup(@()release(UDPReceive));
 
-v = dsp.DynamicFilterVisualizer(8192, 44100, [20 20e3], 'XScale','Log');
+v = dsp.DynamicFilterVisualizer(512, 44100, [20 20e3],'Name','flowEQ','XScale','Log', 'Position',[200,900,400,400]);
 show(v);
 
 %% graphics
 close all;
-latentPlot = figure('Position', [10 10 900 600]);
+latentPlot = figure('Position', [200,500,400,400]);
 
 while true        
     udpr = UDPReceive();
@@ -36,46 +36,48 @@ while true
         z = udpr(44:46);
         dim = udpr(47);
         
-        param_text = {sprintf('Lowshelf Gain:    %0.2f dB', p(1))...
-                      sprintf('Lowshelf Freq:    %0.2f Hz', p(2))...
-                      sprintf('First band Gain:  %0.2f dB', p(3))...
-                      sprintf('First band Freq:  %0.2f Hz', p(4))...
-                      sprintf('First band Q:     %0.2f',    p(5))...
-                      sprintf('Second band Gain: %0.2f dB', p(6))...
-                      sprintf('Second band Freq: %0.2f Hz', p(7))...
-                      sprintf('Second band Q:    %0.2f',    p(8))...
-                      sprintf('Third band Gain:  %0.2f dB', p(9))...
-                      sprintf('Third band Freq:  %0.2f Hz', p(10))...
-                      sprintf('Third band Q:     %0.2f',    p(11))...
-                      sprintf('Highshelf Gain:   %0.2f dB', p(12))...
-                      sprintf('Highshelf Freq:   %0.2f Hz', p(13))};
+        %param_text = {sprintf('Lowshelf Gain:    %0.2f dB', p(1))...
+        %              sprintf('Lowshelf Freq:    %0.2f Hz', p(2))...
+        %              sprintf('First band Gain:  %0.2f dB', p(3))...
+        %              sprintf('First band Freq:  %0.2f Hz', p(4))...
+        %              sprintf('First band Q:     %0.2f',    p(5))...
+        %              sprintf('Second band Gain: %0.2f dB', p(6))...
+        %              sprintf('Second band Freq: %0.2f Hz', p(7))...
+        %              sprintf('Second band Q:    %0.2f',    p(8))...
+        %              sprintf('Third band Gain:  %0.2f dB', p(9))...
+        %              sprintf('Third band Freq:  %0.2f Hz', p(10))...
+        %              sprintf('Third band Q:     %0.2f',    p(11))...
+        %              sprintf('Highshelf Gain:   %0.2f dB', p(12))...
+        %              sprintf('Highshelf Freq:   %0.2f Hz', p(13))};
                       
-        param_text = sprintf('%s\n',param_text{:});
+        %param_text = sprintf('%s\n',param_text{:});
         
-        annotation('textbox',[.7 .5 .5 .2],'String',param_text,'EdgeColor','none','FontName', 'Source Code Variable')
+        %annotation('textbox',[.7 .5 .5 .2],'String',param_text,'EdgeColor','none','FontName', 'Source Code Variable')
         
         v(b,a);
         
-        clf;
+        cla;
         if     dim == 1
             latent_text = sprintf('x = %+0.2f', z(1));
-            
-        elseif dim == 2
-            latent_text = sprintf('x = %+0.2f y = %+0.2f', z(1:2));
-            scatter(z(1), z(2));
+            scatter(z(1), 0, 60, 'r', 'filled');
             xlim([-4 4])
             ylim([-4 4])
+            grid()
+        elseif dim == 2
+            latent_text = sprintf('x = %+0.2f y = %+0.2f', z(1:2));
+            scatter(z(1), z(2), 60, 'r', 'filled');
+            xlim([-4 4])
+            ylim([-4 4])
+            grid()
         else
             latent_text = sprintf('x = %+0.2f y = %+0.2f z = %+0.2f', z);
-            scatter3(z(1), z(2), z(3));
+            scatter3(z(1), z(2), z(3), 60, 'r', 'filled');
             xlim([-4 4])
             ylim([-4 4])
             zlim([-4 4])
         end  
         
         title(latent_text);
-        pos=get(gca,'position');  % retrieve the current values
-        pos(3)=0.7*pos(3);        % try reducing width 30%
-        set(gca,'position',pos);  % write the new values
+        pause(0.05)
     end
 end
